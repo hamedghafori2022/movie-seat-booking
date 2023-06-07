4fa2ba7c22fce42ea928104de8e56899a2c728d0
hamed ghafori <hamedghafori2022@gmail.com>
2023-06-07 17:58:38 +0330

    create script.js file

---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelected = document.getElementById('movie');
+let ticketPrice = +movieSelected.value;
+
+// add event to pick movie
+movieSelected.addEventListener('change', (e)=>{
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+})
+
+// update count and total
+function updateSelectedCount(){
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+container.addEventListener('click',function(e){
+  if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+    
+    e.target.classList.toggle('selected');
+    updateSelectedCount();
+  }
+})
